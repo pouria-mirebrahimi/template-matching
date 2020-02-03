fe1d6ec9a4b7658b47a676cfcb24670e9a31e887
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2020-02-03 16:42:32 +0330

    Bug fix!

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -2,7 +2,7 @@ import glob
 import pytest
 import sys
 
-sys.path.append("../src/gadget/")
+sys.path.append("../src/")
 
 from gadget.files import Images
 from gadget.processing import Preprocessing
